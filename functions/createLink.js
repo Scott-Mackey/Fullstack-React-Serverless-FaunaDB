161322f0294a196e1373da6c468920fc5c58b281
Scott Mackey <scottfmackey@gmail.com>
2020-10-04 04:58:06 +0100

    Added CRUD handling

---
+++ functions/createLink.js
@@ -0,0 +1,17 @@
+const { CREATE_LINK } = require('./utils/linkQueries.js');
+const sendQuery = require('./utils/sendQuery');
+const formattedResponse = require('./utils/formattedResponse');
+
+exports.handler = async(event) => {
+    const { name, url, description } = JSON.parse(event.body);
+    const variables = {name, url, description, archived: false};
+
+    try {
+        const {createLink: createdLink} = await sendQuery(CREATE_LINK, variables);
+
+        return formattedResponse(200, createdLink);
+    } catch (error) {
+        console.error(error);
+        return formattedResponse(500, { error: 'Something went wrong' });
+    }
+}
---
+++ functions/deleteLink.js
@@ -0,0 +1,20 @@
+const { DELETE_LINK } = require('./utils/linkQueries.js');
+const sendQuery = require('./utils/sendQuery');
+const formattedResponse = require('./utils/formattedResponse');
+
+exports.handler = async(event) => {
+    if(event.httpMethod !== 'DELETE') {
+        return formattedResponse(405, { error: 'Method not supported' });
+    }
+    const { _id: id } = JSON.parse(event.body);
+    const variables = {id};
+
+    try {
+        const {deleteLink: deletedLink} = await sendQuery(DELETE_LINK, variables);
+
+        return formattedResponse(200, deletedLink);
+    } catch (error) {
+        console.error(error);
+        return formattedResponse(500, { error: 'Something went wrong' });
+    }
+}
